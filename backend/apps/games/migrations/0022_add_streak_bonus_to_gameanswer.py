df456a0a3a9eb85e551653b6bd7e2253a28f5fde
benoftheworld <benoftheworld84@gmail.com>
2026-03-02 17:36:03 +0100

    Correction

---
+++ backend/apps/games/migrations/0022_add_streak_bonus_to_gameanswer.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("games", "0021_gameplayer_consecutive_correct"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="gameanswer",
+            name="streak_bonus",
+            field=models.IntegerField(default=0, verbose_name="bonus série"),
+        ),
+    ]
